de36ee3969e9aed090a11936c358baae968b0a34
GOJILAB <ts101710@gmail.com>
2025-08-08 11:54:51 +0800

    Create url.js

---
+++ url.js
@@ -0,0 +1,61 @@
+/*
+ * 將要隨機派發的網址放入下方（不需排序）
+ *
+ * 並請注意：
+ * 1. 除最後一項外，其餘項目用逗號分隔
+ * 2. 網址請用引號包起來
+ */
+
+
+const urls = [
+	'https://ytc.mljy.org/113/fortune-poems/01',
+	'https://ytc.mljy.org/113/fortune-poems/02',
+	'https://ytc.mljy.org/113/fortune-poems/03',
+	'https://ytc.mljy.org/113/fortune-poems/04',
+	'https://ytc.mljy.org/113/fortune-poems/05',
+	'https://ytc.mljy.org/113/fortune-poems/06',
+	'https://ytc.mljy.org/113/fortune-poems/07',
+	'https://ytc.mljy.org/113/fortune-poems/08',
+	'https://ytc.mljy.org/113/fortune-poems/09',
+	'https://ytc.mljy.org/113/fortune-poems/10',
+	'https://ytc.mljy.org/113/fortune-poems/11',
+	'https://ytc.mljy.org/113/fortune-poems/12',
+	'https://ytc.mljy.org/113/fortune-poems/13',
+	'https://ytc.mljy.org/113/fortune-poems/14',
+	'https://ytc.mljy.org/113/fortune-poems/15',
+	'https://ytc.mljy.org/113/fortune-poems/16',
+	'https://ytc.mljy.org/113/fortune-poems/17',
+	'https://ytc.mljy.org/113/fortune-poems/18',
+	'https://ytc.mljy.org/113/fortune-poems/19',
+	'https://ytc.mljy.org/113/fortune-poems/20',
+	'https://ytc.mljy.org/113/fortune-poems/21',
+	'https://ytc.mljy.org/113/fortune-poems/22',
+	'https://ytc.mljy.org/113/fortune-poems/23',
+	'https://ytc.mljy.org/113/fortune-poems/24',
+	'https://ytc.mljy.org/113/fortune-poems/25',
+	'https://ytc.mljy.org/113/fortune-poems/26',
+	'https://ytc.mljy.org/113/fortune-poems/27',
+	'https://ytc.mljy.org/113/fortune-poems/28',
+	'https://ytc.mljy.org/113/fortune-poems/29',
+	'https://ytc.mljy.org/113/fortune-poems/30',
+	'https://ytc.mljy.org/113/fortune-poems/31',
+	'https://ytc.mljy.org/113/fortune-poems/32',
+	'https://ytc.mljy.org/113/fortune-poems/33',
+	'https://ytc.mljy.org/113/fortune-poems/34',
+	'https://ytc.mljy.org/113/fortune-poems/35',
+	'https://ytc.mljy.org/113/fortune-poems/36',
+	'https://ytc.mljy.org/113/fortune-poems/37',
+	'https://ytc.mljy.org/113/fortune-poems/38',
+	'https://ytc.mljy.org/113/fortune-poems/39',
+	'https://ytc.mljy.org/113/fortune-poems/40',
+	'https://ytc.mljy.org/113/fortune-poems/41',
+	'https://ytc.mljy.org/113/fortune-poems/42',
+	'https://ytc.mljy.org/113/fortune-poems/43',
+	'https://ytc.mljy.org/113/fortune-poems/44',
+	'https://ytc.mljy.org/113/fortune-poems/45',
+	'https://ytc.mljy.org/113/fortune-poems/46',
+	'https://ytc.mljy.org/113/fortune-poems/47',
+	'https://ytc.mljy.org/113/fortune-poems/48',
+	'https://ytc.mljy.org/113/fortune-poems/49',
+	'https://ytc.mljy.org/113/fortune-poems/50'
+];
